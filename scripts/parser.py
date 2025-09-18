8a89d795956d3dbbb4379b76e49d093125ecc7e8
agentelement <agentelement@agentelement.net>
2025-09-18 01:48:50 -0700

    feat: half a parser

--- scripts/parser.py
+++ scripts/parser.py
@@ -21,6 +21,7 @@ class TokenType(Enum):
     COLON = 16
     EOF = 17
 
+
 class Token:
     def __init__(self, ttype: TokenType, lexeme: str | None, pos: int) -> None:
         self.ttype = ttype
@@ -69,48 +70,48 @@ class Lexer:
         i = 0
         while i < len(input):
             match input[i]:
-                case '(':
+                case "(":
                     self.tokenstream.append(Token(TokenType.LPAREN, None, i))
                     i += 1
-                case ')':
+                case ")":
                     self.tokenstream.append(Token(TokenType.RPAREN, None, i))
                     i += 1
-                case '{':
+                case "{":
                     self.tokenstream.append(Token(TokenType.LBRACE, None, i))
                     i += 1
-                case '}':
+                case "}":
                     self.tokenstream.append(Token(TokenType.RBRACE, None, i))
                     i += 1
-                case '=':
+                case "=":
                     self.tokenstream.append(Token(TokenType.EQ, None, i))
                     i += 1
-                case '-':
+                case "-":
                     self.tokenstream.append(Token(TokenType.MINUS, None, i))
                     i += 1
-                case ',':
+                case ",":
                     self.tokenstream.append(Token(TokenType.COMMA, None, i))
                     i += 1
-                case '*':
+                case "*":
                     self.tokenstream.append(Token(TokenType.STAR, None, i))
                     i += 1
-                case '+':
+                case "+":
                     self.tokenstream.append(Token(TokenType.PLUS, None, i))
                     i += 1
-                case '/':
+                case "/":
                     self.tokenstream.append(Token(TokenType.SLASH, None, i))
                     i += 1
-                case '^':
+                case "^":
                     self.tokenstream.append(Token(TokenType.CARET, None, i))
                     i += 1
-                case ':':
+                case ":":
                     self.tokenstream.append(Token(TokenType.COLON, None, i))
                     i += 1
-                case '\n':
+                case "\n":
                     self.tokenstream.append(Token(TokenType.NEWLINE, None, i))
                     i += 1
-                case ' ' | '\t':
+                case " " | "\t":
                     i += 1
-                case '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' :
+                case "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9":
                     (tok, offset) = self.munch_num(input, i)
                     self.tokenstream.append(tok)
                     i += offset
@@ -125,11 +126,9 @@ class Lexer:
         self.pos += 1
         return token
 
-    def peek(self, i: int) -> Token | None:
-        assert i > 0
-        if self.pos + i > len(self.tokenstream):
-            return None
-
+    def peek(self, i: int) -> Token:
+        assert i >= 0
+        assert self.pos + i <= len(self.tokenstream)
         token = self.tokenstream[self.pos + i - 1]
         return token
 
@@ -142,22 +141,127 @@ class Parser:
         self.vertices = []
         self.faces = []
         self.name = None
-        self.constants = []
+
+        self.constant_exacts = {}
+        self.constant_floats = {}
+        self.constant_sequence = []
+
+    def syntax_error(self):
+        token = self.lexer.peek(0)
+        raise Exception(f"Snytax Eorrr at position {token.pos}")
+
+    @staticmethod
+    def is_expression_ttype(ttype: TokenType) -> bool:
+        match ttype:
+            case (
+                TokenType.INT
+                | TokenType.NAME
+                | TokenType.STAR
+                | TokenType.LPAREN
+                | TokenType.RPAREN
+                | TokenType.SLASH
+            ):
+                return True
+            case _:
+                return False
 
     def expect(self, ttype: TokenType) -> Token:
         token = self.lexer.get()
         if token.ttype != ttype:
-            raise Exception(f"Bad expect at position {token.pos}")
+            raise Exception(
+                f"Expected {ttype} at position {token.pos}, found {token.ttype}"
+            )
 
         return token
+
+    def expect_expression(self) -> Token:
+        token = self.lexer.get()
+        if not self.is_expression_ttype(token.ttype):
+            raise Exception(
+                f"Expected expression token at position {token.pos}, found {token.ttype}"
+            )
+
+        return token
+
+    # name_def := names* \n
+    def name_def(self):
+        names = []
+        while self.lexer.peek(1).ttype == TokenType.NAME:
+            names.append(self.expect(TokenType.NAME).ttype)
+        self.expect(TokenType.NEWLINE)
+        self.name = " ".join(names)
+
+    # constant_def := name = float \n
+    # constant_def := name = float = [int|name|*|(|)|+|/|*|] \n
+    # constant_def := name = [int|name|*|(|)|+|/|] \n
+    def constant_def(self):
+        const = self.expect(TokenType.NAME).lexeme
+        floatv = ""
+        exactv = []
+
+        self.expect(TokenType.EQ)
+        ttype = self.lexer.peek(1).ttype
+        if ttype == TokenType.FLOAT:
+            floatv = self.expect(TokenType.FLOAT).ttype
+            ttype = self.lexer.peek(1).ttype
+
+            if ttype == TokenType.NEWLINE:
+                pass
+            elif ttype == TokenType.EQ:
+                self.expect(TokenType.EQ)
+                while self.is_expression_ttype(self.lexer.peek(1).ttype):
+                    exactv.append(self.expect_expression())
+            else:
+                self.syntax_error()
+
+        elif self.is_expression_ttype(ttype):
+            while self.is_expression_ttype(self.lexer.peek(1).ttype):
+                exactv.append(self.expect_expression())
+
+        self.expect(TokenType.NEWLINE)
+
+        self.constant_sequence.append(const)
+        self.constant_exacts[const] = exactv
+        self.constant_floats[const] = floatv
+
+    # value := -? [name|int|float]
+    def value(self):
+        pass
+
+    # vertex_def := name = (value, value, value) \n
+    def vertex_def(self):
+        pass
+
+    # face_def := { int, int, int } \n
+    def face_def(self):
+        pass
+
+    # constant_block := constant_seq*
+    def constant_block(self):
+        pass
+
+    # vertex_block := vertex_def*
+    def vertex_block(self):
+        pass
+
+    # face_block := Faces : \n face_def*
+    def face_block(self):
+        pass
+
+    # polyhedron := name_def constant_block vertex_block face_block EOF
+    def polyhedron(self):
+        self.name_def()
+        self.constant_block()
+        self.vertex_block()
+        self.expect(TokenType.EOF)
 
 
 def main():
     with open("../data/DisdyakisTriacontahedron.txt") as f:
         parser = Parser(f.read())
-    while parser.lexer.peek(1):
+    while parser.lexer.peek(1).ttype != TokenType:
         print(parser.lexer.get().ttype)
+
 
 if __name__ == "__main__":
     main()
-
