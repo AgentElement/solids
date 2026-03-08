48fdf972dc46f6e4e3fa432f3e9fc4fc5441c559
agentelement <agentelement@agentelement.net>
2026-03-07 19:44:52 -0700

    build: nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Generate print-in-place mesh assemblies";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ pkgs.uv ];
+
+        # C libraries for numpy,
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          pkgs.stdenv.cc.cc.lib
+          pkgs.zlib
+          pkgs.glib
+        ];
+      };
+    };
+}
